51b97e7cbf2c655ebe8be6f173e036021b41eecf
Fran Babić <fran.babic1907@gmail.com>
2025-09-22 21:55:41 +0200

    init

---
+++ client.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::clog << "Client binary started" << std::endl;
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::clog << "Server binary started" << std::endl;
+    return 0;
+}
