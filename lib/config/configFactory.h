6cc6abe9b932f2fe788945097ccfecaa4fc52986
Fran Babic <fran.babic1907@gmail.com>
2025-12-02 12:28:35 +0100

    cosmetic

--- lib/config/configFactory.h
+++ lib/config/configFactory.h
@@ -8,11 +8,7 @@ namespace config {
 
 class ConfigFactory {
 public:
-    // Load a configuration from an XML file. Returns a pointer to a heap
-    // allocated Config instance; caller is responsible for its lifetime.
     static std::shared_ptr<Config> loadFromFile(const std::string& path);
-
-    // Parse configuration from an XML string. Same ownership rules.
     static std::shared_ptr<Config> loadFromString(const std::string& xml);
 };
 
